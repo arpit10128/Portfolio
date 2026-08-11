c891ccc7f6127450d9cc5d38da82b1a1c0d113da
arpit10128 <arpit10128@gmail.com>
2026-08-12 04:56:08 +0530

    fix and update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import dayjs from "dayjs";
 
-import { navIcons, navLinks } from "#constants";
+import { navLinks } from "#constants";
 
 const Navbar = () => {
   return (
     <nav>
       <div>
-        <img src="/images/logo.svg" alt="logo" />
-        <p className="font-bold">Arpit's Portfolio</p>
+        <p className="text-white font-bold">
+          Arpit's Portfolio
+        </p>
 
-        <ul>
+        <ul className="text-white">
           {navLinks.map(({ id, name }) => (
             <li key={id}>
               <p>{name}</p>
@@ -19,7 +20,7 @@ const Navbar = () => {
       </div>
 
       <div>
-        <ul>
+        {/* <ul>
           {navIcons.map(({ id, img }) => (
             <li key={id}>
               <img
@@ -29,7 +30,7 @@ const Navbar = () => {
               />
             </li>
           ))}
-        </ul>
+        </ul> */}
 
         <time datetime="">
           {dayjs().format("ddd MMM D h:mm A")}

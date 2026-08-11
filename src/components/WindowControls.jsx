4de16f9dd8a61aecdcbeea8ffb66c20b80b6765d
arpit10128 <arpit10128@gmail.com>
2026-08-12 02:57:40 +0530

    fix button

--- src/components/WindowControls.jsx
+++ src/components/WindowControls.jsx
@@ -4,8 +4,10 @@ const WindowControls = ({ target }) => {
   const { closeWindow } = useWindowStore();
   return (
     <div id="window-controls">
-      <div
+      <button
+        type="button"
         className="close"
+        aria-label="close window"
         onClick={() => closeWindow(target)}
       />
       <div className="minimize" />

3e41d7900834960a7da90682c6dfa293138ea009
arpit10128 <arpit10128@gmail.com>
2026-08-12 12:15:06 +0530

    implement safari window

---
+++ src/windows/Safari.jsx
@@ -0,0 +1,62 @@
+import { WindowControls } from "#components";
+import { blogPosts } from "#constants";
+import { MoveRight } from "lucide-react";
+import WindowWrapper from "../hoc/WindowWrapper";
+
+const Safari = () => {
+  return (
+    <>
+      <div
+        id="window-header"
+        style={{
+          position: "relative",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <WindowControls target="safari" />
+
+        <div
+          style={{
+            position: "absolute",
+            left: "50%",
+            transform: "translateX(-50%)",
+          }}
+        >
+          <p>Articles</p>
+        </div>
+      </div>
+
+      <div className="blog">
+        <h2>Blogs</h2>
+        <div className="space-y-8 blog-list">
+          {blogPosts.map(
+            ({ id, image, title, date, link }) => (
+              <div key={id} className="blog-post">
+                <div className="col-span-2">
+                  <img src={image} alt={title} />
+                </div>
+                <div className="content">
+                  <p>{date}</p>
+                  <h3>{title}</h3>
+                  <a
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Check out{" "}
+                    <MoveRight className="icon-hover" />
+                  </a>
+                </div>
+              </div>
+            ),
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+const SafariWindow = WindowWrapper(Safari, "safari");
+
+export default SafariWindow;
